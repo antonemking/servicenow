44b25f66e474d47bbf3042b9de90c521caf481bd
Antone king <toneking972@gmail.com>
2016-04-14 11:17:19 -0400

    Update findReplace.js

--- Background-Scripts/findReplace.js
+++ Background-Scripts/findReplace.js
@@ -1,14 +1,21 @@
-//Replace 'table name here','query here if needed','field name here' ith your parameters for the comma remove
-removeCommas('table name here','query here if needed','field name here');
-function removeCommas(rcTableName,rcEncodedQuery,rcFieldName) {
- var grCommaRemove = new GlideRecord(rcTableName);
- grCommaRemove.addEncodedQuery(rcEncodedQuery)
- grCommaRemove.query();
- var updateCount = 0;
- while(grCommaRemove.next()){
- grCommaRemove[rcFieldName] = grCommaRemove[rcFieldName].replace(/,/g , "");
- grCommaRemove.update();
- updateCount++;
- }
- gs.print('Records Updated: '+ updateCount);
+//Replace 'table name here','query here if needed','field name here' ith your parameters for the findreplace
+
+removeAnom('table', 'encodedquery', 'element field')
+function removeAnom(table, query, field){
+    var gr = new GlideRecord(table);
+    gr.addEncodedQuery(query);
+    gr.query();
+    var updateCount = 0;
+    while(gr.next()){
+        gr[field] = gr[field].replace(/ (what do you want to find?/g, "'");
+        gr.update();
+        updateCount++;
+
+
+    }
+gs.print('Records updated: ' + updateCount);
+
+
+
+
 }
