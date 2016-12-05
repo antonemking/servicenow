dea42013862b9f0b38e22af2031444b15dbcb9fb
Tone King <toneking972@gmail.com>
2016-12-05 15:55:14 -0500

    includes

--- Script_Includes/eventUtils.js
+++ Script_Includes/eventUtils.js
@@ -3,12 +3,41 @@ eventUtils.prototype = {
     initialize: function() {
     },
 	
-	getRecords: function(){
-		var query = 'sys_created_onONToday@javascript:gs.daysAgoStart(0)@javascript:gs.daysAgoEnd(0)';
+	/*
+	Query for events...Seperate functions incase querys for markets or segments need to be customized more.
+	*/
+	
+	getRecordsBySegment: function(segment, startTime, endTime){
+		//var query = 'sys_created_onONLast ' + startTime + ' days@javascript:gs.daysAgoStart(' + startTime + ')';
+		//query += '@javascript:gs.daysAgoEnd(' + endTime + ')';
+		//query += '^country.x_scafe_major_app_market_segment=' + segment;
 		var gr = new GlideRecord('x_scafe_major_app_event');
-		gr.addEncodedQuery(query);
+		gr.addQuery('sys_created_on', '>=', gs.hoursAgo(endTime));
+		gr.addQuery('sys_created_on' , '<=', gs.hoursAgo(startTime));
+		//gr.addEncodedQuery(query);
 		gr.query();
 		return gr;
+	},
+	
+	getRecordsByMarket: function(market, startTime, endTime){
+		//var query = 'sys_created_onONToday@javascript:gs.daysAgoStart(0)@javascript:gs.daysAgoEnd(0)^market=' + market;
+		var gr = new GlideRecord('x_scafe_major_app_event');
+		gr.addQuery('sys_created_on', '>=', gs.hoursAgo(endTime));
+		gr.addQuery('sys_created_on' , '<=', gs.hoursAgo(startTime));
+		//gr.addEncodedQuery(query);
+		gr.query();
+		return gr;
+	},
+	
+	getRecordsAll: function(startTime, endTime){
+		//var query = 'sys_created_onONToday@javascript:gs.daysAgoStart(0)@javascript:gs.daysAgoEnd(0)';
+		var gr = new GlideRecord('x_scafe_major_app_event');
+		gr.addQuery('sys_created_on', '>=', gs.hoursAgo(endTime));
+		gr.addQuery('sys_created_on' , '<=', gs.hoursAgo(startTime));
+		//gr.addEncodedQuery(query);
+		gr.query();
+		return gr;
+		
 	},
 	
 	getEventCount: function(records){
@@ -22,12 +51,14 @@ eventUtils.prototype = {
 		fieldId.next();
 		return fieldId.sys_id;
 	},
-	
-	errorDetection: function(table, subject){
+	/*
+	Detect if an rfm alert was created with the same subject and the incident record is still active.
+	*/
+	rfmErrorDetection: function(table, subject){
 		var value = false;
 		var e = new GlideRecord(table);
 		var query = 'short_description='+subject;
-		query += '^active=true';
+		query += '^active=true^state!=6';
 		e.addEncodedQuery(query);
 		e.query();
 		if(e.next()){
@@ -36,6 +67,21 @@ eventUtils.prototype = {
 		}
 		return value;
 		
+	},
+	/*
+	Detect if a ping alert was created in the last hour with the same subject.
+	*/
+	pingErrorDetection: function(table, subject){
+		var value = false;
+		var e = new GlideRecord(table);
+		var query = 'sys_created_onRELATIVEGE@hour@ago@1';
+		query += '^active=true^state!=6^short_description='+subject;
+		e.addEncodedQuery(query);
+		e.query();
+			if(e.next()){
+				value = true;
+			}
+		return value;
 	},
 	
 	getTicketStatus: function(eRecords){
@@ -55,10 +101,13 @@ eventUtils.prototype = {
 	
 	
 	},
-	
+	/*
+	 Mobile API Response
+	*/
 	
 	populateResponse: function(events){
 		var resp={};
+		var market = request.pathParams.market_seg;
 			
 		function ticketStatus(){
 					var inc = new GlideRecord('incident');
@@ -69,9 +118,19 @@ eventUtils.prototype = {
 							return {"short_desc":inc.getValue('short_description'),
 								   "state": inc.state.getDisplayValue(),
 								   "desc": inc.getValue('description'),
-								   "number": inc.getValue('number')};
+								   "number": inc.getValue('number'),
+								   "priority": inc.getValue('priority')};
 						}
 				}
+		function getSegment(){
+			var country = events.getValue('country');
+			var seg = new GlideRecord('cmn_location');
+			seg.addQuery('sys_id', country);
+			seg.query();
+			if(seg.next()){
+				return seg.getValue('x_scafe_major_app_market_segment');
+			}
+		}
 		while (events.next()) {
 			var clazz = events.getTableName();
 			var sys_id = events.getValue('sys_id');
@@ -89,7 +148,8 @@ eventUtils.prototype = {
 				system_status: events.getDisplayValue('system_status'),
 				event: events.getValue('event'),
 				alert_type: events.getValue('alert_type'),
-				coutry: events.getValue('country'),
+				country: events.getDisplayValue('country'),
+				segment: getSegment(),
 				market: events.getValue('market'),
 				status: events.getDisplayValue('status'),
 				count: events.getValue('count'),
